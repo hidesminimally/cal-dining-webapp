0565f80298c56eb836990b25bf3225290b7b23d3
Shimmy Li <danielmxli@gmail.com>
2015-03-20 01:10:41 -0700

    Implement first alert view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^cookie_alert/', include('cookie_alert.urls'), name='cookie_alert_index'),
+    
 )
